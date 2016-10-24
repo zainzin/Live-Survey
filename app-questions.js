c63d4dec1b440e73022e688e460a4da2d7d4b96b
Zain Hatim <zain.hatim@gmail.com>
2016-10-24 12:18:30 -0400

    init

---
+++ app-questions.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        "q": "Overall, I am very satisfied with the way Company performed on this project?",
+        "a": "Strongly Disagree",
+        "b": "Somewhat Disagree",
+        "c": "Somewhat Agree",
+        "d": "Strongly Agree"
+    },
+    {
+        "q": "Overall, I am satisfied with the [Company] service representatives (CSRs)?",
+        "a": "Strongly Disagree",
+        "b": "Somewhat Disagree",
+        "c": "Somewhat Agree",
+        "d": "Strongly Agree"
+    },
+    {
+        "q": "The Company account manager is making a positive contribution to my business?",
+        "a": "Strongly Disagree",
+        "b": "Somewhat Disagree",
+        "c": "Somewhat Agree",
+        "d": "Strongly Agree"
+    },
+    {
+        "q": "Compared to how you felt about the Company before this project, what is the likelihood of completing another project with our Company?",
+        "a": "Strongly Disagree",
+        "b": "Somewhat Disagree",
+        "c": "Somewhat Agree",
+        "d": "Strongly Agree"
+    }
+];
